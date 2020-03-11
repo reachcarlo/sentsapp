05a7c7709837a5087aea213c6eb4eac01ef6c842
Carlo Tapia <carlotapia@carlos-mac-mini.lan>
2020-03-11 07:23:23 -0700

    routes

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/costs', require('./routes/api/auth.js'));
+app.use('/api/restaurants', require('./routes/api/auth.js'));
+app.use('/api/users', require('./routes/api/auth.js'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
